69f85ed619fba9b05d7bff239b45a43c910c68ab
ThangNQ16 <ThangNQ16@fpt.com>
2024-03-15 16:10:06 +0700

    Mar.15 0.0.1

--- Meme.V1/ViewController.swift
+++ Meme.V1/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imagePickerView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        UIActivityViewController
     }
-
-
+    
+    @IBAction func captureAnImage(_ sender: Any) {
+        let takeImageController = UIImagePickerController()
+        takeImageController.sourceType = .camera
+        takeImageController.delegate = self
+        present(takeImageController, animated: true)
+    }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickImageController = UIImagePickerController()
+        pickImageController.delegate = self
+        present(pickImageController, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true)
+        self.imagePickerView.image = info[.originalImage] as? UIImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
 }
 
