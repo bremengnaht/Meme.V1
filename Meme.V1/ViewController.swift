ee92c21483ce059a450357b36078fa3b9bc14350
ThangNQ16 <ThangNQ16@fpt.com>
2024-03-16 10:48:41 +0700

    Mar.16-0.0.3

--- Meme.V1/ViewController.swift
+++ Meme.V1/ViewController.swift
@@ -11,34 +11,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet var imagePickerView: UIImageView!
     @IBOutlet var cameraButton: UIBarButtonItem!
+    @IBOutlet var topTextField: UITextField!
+    @IBOutlet var bottomTextField: UITextField!
     
     let imagePickerController = UIImagePickerController()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imagePickerController.delegate = self
         
         // Doesn't work on Apple Silicon Macs. This return TRUE even on simulator.
         self.cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        self._configTextFieldStyle(textField: self.topTextField, defaultText: "TOP")
+        self._configTextFieldStyle(textField: self.bottomTextField, defaultText: "BOTTOM")
     }
     
     @IBAction func captureAnImage(_ sender: Any) {
         self.imagePickerController.sourceType = .camera
-        present(self.imagePickerController, animated: true)
+        self.present(self.imagePickerController, animated: true)
     }
     
     @IBAction func pickAnImage(_ sender: Any) {
         self.imagePickerController.sourceType = .photoLibrary
-        present(self.imagePickerController, animated: true)
+        self.present(self.imagePickerController, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        dismiss(animated: true)
+        self.dismiss(animated: true)
         self.imagePickerView.image = info[.originalImage] as? UIImage
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true)
+        self.dismiss(animated: true)
+    }
+    
+    private func _configTextFieldStyle(textField: UITextField, defaultText: String) {
+        let memeTextAttributes: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "Impact", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  3,
+        ]
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.autocapitalizationType = .allCharacters
+        textField.borderStyle = .none
+        textField.textAlignment = .center
+        textField.text = defaultText
+        textField.minimumFontSize = 12
     }
     
 }
