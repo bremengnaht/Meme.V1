e35e37235129750693836639fee2495ad7a2f93a
ThangNQ16 <ThangNQ16@fpt.com>
2024-03-16 14:19:02 +0700

    Mar.16-0.0.4

--- Meme.V1/ViewController.swift
+++ Meme.V1/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
     @IBOutlet var imagePickerView: UIImageView!
     @IBOutlet var cameraButton: UIBarButtonItem!
@@ -23,8 +23,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Doesn't work on Apple Silicon Macs. This return TRUE even on simulator.
         self.cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         
+        self.topTextField.delegate = self
+        self.bottomTextField.delegate = self
+        
         self._configTextFieldStyle(textField: self.topTextField, defaultText: "TOP")
         self._configTextFieldStyle(textField: self.bottomTextField, defaultText: "BOTTOM")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
     
     @IBAction func captureAnImage(_ sender: Any) {
@@ -46,12 +61,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.dismiss(animated: true)
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" {
+            textField.text = nil
+        }
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        view.frame.origin.y -= self._getKeyboardHeight(notification);
+    }
+    
     private func _configTextFieldStyle(textField: UITextField, defaultText: String) {
         let memeTextAttributes: [NSAttributedString.Key : Any] = [
             NSAttributedString.Key.strokeColor: UIColor.black,
             NSAttributedString.Key.foregroundColor: UIColor.white,
             NSAttributedString.Key.font: UIFont(name: "Impact", size: 40)!,
-            NSAttributedString.Key.strokeWidth:  3,
+            NSAttributedString.Key.strokeWidth:  -5,
         ]
         textField.defaultTextAttributes = memeTextAttributes
         textField.autocapitalizationType = .allCharacters
@@ -59,6 +84,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         textField.textAlignment = .center
         textField.text = defaultText
         textField.minimumFontSize = 12
+    }
+    
+    func _getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        return (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
     }
     
 }
